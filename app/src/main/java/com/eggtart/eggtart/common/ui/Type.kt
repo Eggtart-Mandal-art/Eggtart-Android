b04b59fd39e16a9681e3df4f90c2d5bb226be185
노원진 <wonjin2168@gmail.com>
2024-03-25 16:39:14 +0900

    #4 Main화면 작업중
    - BottomNavigationBar 작업 완료
    - AppBar 작업 완료
    - Pretendard Font 추가

--- app/src/main/java/com/eggtart/eggtart/common/ui/Type.kt
+++ app/src/main/java/com/eggtart/eggtart/common/ui/Type.kt
@@ -1,34 +1,41 @@
 package com.eggtart.eggtart.common.ui
 
 import androidx.compose.material3.Typography
-import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.eggtart.eggtart.R
+
+private val DefaultTypography = Typography()
+
+private val DefaultFontFamily = FontFamily(
+    Font(weight = FontWeight.Black, resId = R.font.pretendard_black),
+    Font(weight = FontWeight.ExtraBold, resId = R.font.pretendard_extra_bold),
+    Font(weight = FontWeight.Bold, resId = R.font.pretendard_bold),
+    Font(weight = FontWeight.SemiBold, resId = R.font.pretendard_semi_bold),
+    Font(weight = FontWeight.Medium, resId = R.font.pretendard_medium),
+    Font(weight = FontWeight.Normal, resId = R.font.pretendard_regular),
+    Font(weight = FontWeight.Light, resId = R.font.pretendard_light),
+    Font(weight = FontWeight.ExtraLight, resId = R.font.pretendard_extra_light),
+    Font(weight = FontWeight.Thin, resId = R.font.pretendard_thin),
+)
 
 // Set of Material typography styles to start with
 val Typography = Typography(
-    bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
-    ),
-    labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
-    )
-    */
+    displayLarge = DefaultTypography.displayLarge.copy(fontFamily = DefaultFontFamily),
+    displayMedium = DefaultTypography.displayMedium.copy(fontFamily = DefaultFontFamily),
+    displaySmall = DefaultTypography.displaySmall.copy(fontFamily = DefaultFontFamily),
+    headlineLarge = DefaultTypography.headlineLarge.copy(fontFamily = DefaultFontFamily),
+    headlineMedium = DefaultTypography.headlineMedium.copy(fontFamily = DefaultFontFamily),
+    headlineSmall = DefaultTypography.headlineSmall.copy(fontFamily = DefaultFontFamily),
+    titleLarge = DefaultTypography.titleLarge.copy(fontFamily = DefaultFontFamily, fontSize = 20.sp, lineHeight = 24.sp, fontWeight = FontWeight.Bold),
+    titleMedium = DefaultTypography.titleMedium.copy(fontFamily = DefaultFontFamily),
+    titleSmall = DefaultTypography.titleSmall.copy(fontFamily = DefaultFontFamily),
+    bodyLarge = DefaultTypography.bodyLarge.copy(fontFamily = DefaultFontFamily),
+    bodyMedium = DefaultTypography.bodyMedium.copy(fontFamily = DefaultFontFamily),
+    bodySmall = DefaultTypography.bodySmall.copy(fontFamily = DefaultFontFamily),
+    labelLarge = DefaultTypography.labelLarge.copy(fontFamily = DefaultFontFamily),
+    labelMedium = DefaultTypography.labelMedium.copy(fontFamily = DefaultFontFamily, fontSize = 12.sp, lineHeight = 18.sp, fontWeight = FontWeight.Medium),
+    labelSmall = DefaultTypography.labelSmall.copy(fontFamily = DefaultFontFamily)
 )
--- app/src/main/java/com/eggtart/eggtart/features/main/MainActivity.kt
+++ app/src/main/java/com/eggtart/eggtart/features/main/MainActivity.kt
@@ -4,22 +4,34 @@ import android.annotation.SuppressLint
 import androidx.activity.viewModels
 import androidx.annotation.DrawableRes
 import androidx.annotation.StringRes
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Star
+import androidx.compose.material3.DividerDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarDefaults
 import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.NavigationBarItemColors
+import androidx.compose.material3.NavigationBarItemDefaults
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
@@ -28,6 +40,10 @@ import androidx.navigation.compose.rememberNavController
 import com.eggtart.eggtart.R
 import com.eggtart.eggtart.common.BaseActivity
 import com.eggtart.eggtart.common.ui.EggtartTheme
+import com.eggtart.eggtart.features.main.screens.calendar.CalendarScreen
+import com.eggtart.eggtart.features.main.screens.mandalart.MandalartScreen
+import com.eggtart.eggtart.features.main.screens.settings.SettingsScreen
+import com.eggtart.eggtart.features.main.screens.timer.TimerScreen
 import dagger.hilt.android.AndroidEntryPoint
 
 /**
@@ -54,7 +70,7 @@ class MainActivity : BaseActivity() {
 @Composable
 private fun MainAppBar() {
     TopAppBar(title = {
-        Text(text = "에그타르트")
+        Text(text = stringResource(id = R.string.app_name), style = MaterialTheme.typography.titleLarge)
     }, actions = {
         Icon(imageVector = Icons.Filled.Star, contentDescription = "")
     })
@@ -69,34 +85,47 @@ private fun BottomNavBar(navController: NavHostController) {
         BottomNavItem.Settings
     )
 
-    NavigationBar {
-        val navBackStackEntry by navController.currentBackStackEntryAsState()
-        val currentRoute = navBackStackEntry?.destination?.route
+    Column {
+        HorizontalDivider()
+        NavigationBar(containerColor = MaterialTheme.colorScheme.background, tonalElevation = 0.dp) {
+            val navBackStackEntry by navController.currentBackStackEntryAsState()
+            val currentRoute = navBackStackEntry?.destination?.route
 
-        items.forEach {
-            NavigationBarItem(
-                selected = currentRoute == it.screenRoute,
-                onClick = {
-                    navController.navigate(it.screenRoute) {
-                        navController.graph.startDestinationRoute?.let { route ->
-                            popUpTo(route) {
-                                saveState = true
+            items.forEach {
+                NavigationBarItem(
+                    modifier = Modifier.clip(RoundedCornerShape(24.dp)),
+                    selected = currentRoute == it.screenRoute,
+                    onClick = {
+                        navController.navigate(it.screenRoute) {
+                            navController.graph.startDestinationRoute?.let { route ->
+                                popUpTo(route) {
+                                    saveState = true
+                                }
                             }
+                            launchSingleTop = true
+                            restoreState = true
                         }
-                        launchSingleTop = true
-                        restoreState = true
-                    }
-                },
-                icon = {
-                    Icon(
-                        modifier = Modifier.alpha(if (currentRoute == it.screenRoute) 0.7f else 0.25f),
-                        painter = painterResource(id = it.iconId),
-                        contentDescription = stringResource(id = it.labelId)
+                    },
+                    icon = {
+                        Icon(
+                            painter = painterResource(id = it.iconId),
+                            contentDescription = stringResource(id = it.labelId)
+                        )
+                    },
+                    label = {
+                        Text(text = stringResource(id = it.labelId))
+                    },
+                    colors = NavigationBarItemColors(
+                        selectedIconColor = MaterialTheme.colorScheme.onBackground.copy(alpha = 0.7f),
+                        selectedTextColor = MaterialTheme.colorScheme.onBackground.copy(alpha = 0.7f),
+                        selectedIndicatorColor = Color.Transparent,
+                        unselectedIconColor = MaterialTheme.colorScheme.onBackground.copy(alpha = 0.25f),
+                        unselectedTextColor = MaterialTheme.colorScheme.onBackground.copy(alpha = 0.25f),
+                        disabledIconColor = MaterialTheme.colorScheme.onBackground.copy(alpha = 0.1f),
+                        disabledTextColor = MaterialTheme.colorScheme.onBackground.copy(alpha = 0.1f)
                     )
-                },
-                label = {
-                    Text(text = stringResource(id = it.labelId))
-                })
+                )
+            }
         }
     }
 }
@@ -105,26 +134,26 @@ private fun BottomNavBar(navController: NavHostController) {
 private fun MainBody(navController: NavHostController) {
     NavHost(navController = navController, startDestination = BottomNavItem.Mandalart.screenRoute) {
         composable(BottomNavItem.Mandalart.screenRoute) {
-
+            MandalartScreen()
         }
         composable(BottomNavItem.Calendar.screenRoute) {
-
+            CalendarScreen()
         }
         composable(BottomNavItem.Timer.screenRoute) {
-
+            TimerScreen()
         }
         composable(BottomNavItem.Settings.screenRoute) {
-
+            SettingsScreen()
         }
     }
 }
 
 
 sealed class BottomNavItem(@StringRes val labelId: Int, @DrawableRes val iconId: Int, val screenRoute: String) {
-    object Mandalart : BottomNavItem(R.string.nav_mandalart, R.drawable.ic_mandalart_n, "/Mandalart")
-    object Calendar : BottomNavItem(R.string.nav_calendar, R.drawable.ic_calendar_n, "/Calendar")
-    object Timer : BottomNavItem(R.string.nav_timer, R.drawable.ic_timer_n, "/Timer")
-    object Settings : BottomNavItem(R.string.nav_settings, R.drawable.ic_settings_n, "/Settings")
+    data object Mandalart : BottomNavItem(R.string.nav_mandalart, R.drawable.ic_mandalart_n, "/Mandalart")
+    data object Calendar : BottomNavItem(R.string.nav_calendar, R.drawable.ic_calendar_n, "/Calendar")
+    data object Timer : BottomNavItem(R.string.nav_timer, R.drawable.ic_timer_n, "/Timer")
+    data object Settings : BottomNavItem(R.string.nav_settings, R.drawable.ic_settings_n, "/Settings")
 }
 
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
@@ -132,11 +161,12 @@ sealed class BottomNavItem(@StringRes val labelId: Int, @DrawableRes val iconId:
 @Composable
 fun GreetingPreview() {
     EggtartTheme {
+        val navController = rememberNavController()
         Scaffold(
             topBar = { MainAppBar() },
-            bottomBar = { BottomNavBar(rememberNavController()) },
+            bottomBar = { BottomNavBar(navController) },
         ) {
-            MainBody(rememberNavController())
+            MainBody(navController)
         }
     }
 }
